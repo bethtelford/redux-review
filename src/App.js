5e1fa5d61e0c503ea63ed69873ad1f817a0e3800
bethtelford <mrsbethtelford@gmail.com>
2018-02-19 13:31:47 -0700

    testing git creds pt6

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Testing git creds pt5</p>
+        <p>Testing git creds pt6</p>
         <Dummy testProp={this.state.test}/>
       </div>
     );
